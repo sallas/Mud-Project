8954ea4cfb3b133ac2779f0adc0b35e326320502
Tobias karlsson <tbc105@hotmail.com>
2014-03-06 21:00:36 +0100

    Upgrade the fighting system

--- src/gitmudproject/GameLogic.java
+++ src/gitmudproject/GameLogic.java
@@ -7,7 +7,6 @@ public class GameLogic
     private Player player;
     private Room room;
     private MonsterList monsterList;
-    private Monster monster;
    
     
     public void startGame(Player player)
@@ -24,37 +23,67 @@ public class GameLogic
         Scanner scan = new Scanner(System.in);
         while(player.getHealth() > 0)
         {
-            this.monster = monsterList.choseMonster();
+            Monster monster = new Monster(30, 10, "TEMPNAME");
             room = new Room(monster);
             System.out.println("you see: "+monster.getName());
-            if(fightMonster(monster) != true)
+            if(engagetMonster(monster) != true)
                 break;              // Player died
             System.out.print("Would you like to move to the next room? (y/n): ");
             if("n".equalsIgnoreCase(scan.next()))
                 break;
+            System.out.println("\n\n");
         }
         System.out.println("Good bye");
     }
     
-    private boolean fightMonster(Monster monster)
+    private boolean engagetMonster(Monster monster)
     {
+        Scanner scan = new Scanner(System.in);
         while(player.getHealth() > 0 && monster.getHealth() > 0)
         {
-            player.setHealth(player.getHealth() - monster.getDamage());
-            if(player.getHealth() > 0)
+            boolean done = false;
+            while(!done)
             {
-                monster.setHealth(monster.getHealth() - player.getDamage());
-                if(monster.getHealth() <= 0)
+                done = true;
+                System.out.println("What would you like to do(write the corresponding number)"
+                        + "         \n1: Attack\n2: Drink potion\n3: Use spell");
+                int option = scan.nextInt();
+                switch(option)
                 {
-                    System.out.println("You have slayed " + monster.getName());
+                    case 1: fightMonster(monster);
+                        break;
+                    case 2: System.out.println("That feature is not implemented");
+                        break;
+                    case 3: System.out.println("That feature is not implemented");
+                        break;
+                    default: System.out.println("That wasn't an option");
+                        done = false;
+                        break;
                 }
-            }
-            else
-            {
-                System.out.println("You died");
             }
             
         }
         return player.getHealth() > 0;  // return true if player is alive
+    }
+
+    private void fightMonster(Monster monster)
+    {
+            
+            monster.setHealth(monster.getHealth() - player.getDamage());
+            System.out.println("You dealt " + player.getDamage() + " to " + monster.getName());
+            if(monster.getHealth() <= 0)
+            {
+                System.out.println("\nYou have slayed " + monster.getName());
+            }
+            else
+            {
+                player.setHealth(player.getHealth() - monster.getDamage());
+                System.out.println(monster.getName() + " dealt " + monster.getDamage() + " to you");
+                if(player.getHealth() <= 0)
+                {
+                   System.out.println("\nYou died");
+                }
+            }
+            System.out.println("You've got " + player.getHealth() + " health remaining\n");
     }
 }
